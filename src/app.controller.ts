0b01fc0e8b529ef1b59232cfa57b939278f83abd
qkaidev1510 <truongquangkhai151020@gmail.com>
2025-09-29 18:03:19 +0700

    fix: healthcheck path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,16 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Public()
+  @Get()
+  getRoot() {
+    return { status: 'OK' };
+  }
+
+  @Public()
+  @Head()
+  root() {}
+
+  @Public()
   @Get('health')
   getCheck() {
     return { status: 'OK' };
